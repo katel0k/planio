5817e24c0ca4e005ac465bf2a8f1dfb99ff63a22
katel0k <katelkin2003@mail.ru>
2024-12-25 16:08:06 +0300

    prepared database for simple chatter

--- planbook/ddl.sql
+++ planbook/ddl.sql
@@ -1,10 +1,19 @@
 START TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nickname VARCHAR(32) NOT NULL
+);
+
 CREATE TABLE plans (
     id SERIAL PRIMARY KEY,
+    author_id INTEGER NOT NULL,
     synopsis VARCHAR(128) NOT NULL,
     creation_dttm TIMESTAMP DEFAULT NOW()::TIMESTAMP
 );
+
+ALTER TABLE plans ADD CONSTRAINT FK_users_plans FOREIGN KEY
+    (author_id) REFERENCES users(id);
 
 CREATE TABLE descriptions (
     plan_id INTEGER PRIMARY KEY,
@@ -13,5 +22,17 @@ CREATE TABLE descriptions (
 
 ALTER TABLE descriptions ADD CONSTRAINT FK_description_for_plan FOREIGN KEY
     (plan_id) REFERENCES plans(id);
+
+CREATE TABLE messages (
+    author_id INTEGER,
+    receiver_id INTEGER,
+    sent_dttm TIMESTAMP DEFAULT NOW()::TIMESTAMP,
+    body VARCHAR(1024)
+);
+
+ALTER TABLE messages ADD CONSTRAINT FK_users_messages FOREIGN KEY
+    (author_id) REFERENCES users(id);
+ALTER TABLE messages ADD CONSTRAINT FK_messages_to_user FOREIGN KEY
+    (receiver_id) REFERENCES users(id);
 
 COMMIT;
---
+++ plango/go.mod
@@ -0,0 +1,3 @@
+module github.com/katel0k/planio
+
+go 1.23.4
