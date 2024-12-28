e87f08b3e435ede3cd04d2dd821beb755aa7dc0a
katel0k <katelkin2003@mail.ru>
2024-12-28 16:28:50 +0300

    Context magic go brrrrrrrrrrr

--- plango/mock_client/main.go
+++ plango/mock_client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -23,87 +24,17 @@ import (
 
 var PONG_TIMEOUT time.Duration = time.Second * 5
 
-type command int
-
-const (
-	SERVER_ERROR command = iota
-)
-
-var shutdownChannel chan command
-
-func pingPong(c *http.Client, pingURL *url.URL, printPingInfo bool) {
-	defer func() { shutdownChannel <- SERVER_ERROR }()
-	buffer := make([]byte, 1024)
-	for {
-		pong, err := c.Get(pingURL.String())
-
-		if err != nil {
-			log.Println(err)
-			log.Println("Server error, quitting")
-			return
-		}
-		var waiter chan []byte = make(chan []byte)
-		go (func() {
-			defer pong.Body.Close()
-			n, _ := pong.Body.Read(buffer)
-			if n != 0 {
-				waiter <- buffer[0:n]
-			}
-		})()
-		buf := <-waiter
-		msg := msg_pb.MsgResponse{}
-		err = proto.Unmarshal(buf, &msg)
-		if err == nil {
-			log.Printf("%d received msg %d: %s", msg.AuthorId, msg.Id, msg.Text)
-		} else if string(buf) == "pong" {
-			if printPingInfo {
-				log.Printf("pong")
-			}
-		} else {
-			log.Print(err)
-		}
-	}
-}
-
-func main() {
-	printPingInfo := flag.Bool("d", false, "Log ping pong debug info")
-	flag.Parse()
-
+func interactWithUser(ctx context.Context, c *http.Client) {
 	SERVER_URL, _ := url.Parse("http://0.0.0.0:5000")
-	joinURL := SERVER_URL.JoinPath("/join/mock")
-	pingURL := SERVER_URL.JoinPath("/ping")
 	msgURL := SERVER_URL.JoinPath("/message")
 	allUsersURL := SERVER_URL.JoinPath("/users")
 	allPlansURL := SERVER_URL.JoinPath("/plans")
 	newPlanURL := SERVER_URL.JoinPath("/plan")
-
-	jar, _ := cookiejar.New(nil)
-	c := http.Client{
-		Timeout: PONG_TIMEOUT,
-		Jar:     jar,
-	}
-	defer c.CloseIdleConnections()
-	resp, err := c.Get(joinURL.String())
-	if err != nil {
-		log.Println(err)
-		log.Println("Server error, quitting")
-		return
-	}
-	c.Jar.SetCookies(joinURL, resp.Cookies())
 	buffer := make([]byte, 1024)
-	n, err := resp.Body.Read(buffer)
-	if err != io.EOF && n != 0 {
-		log.Println(err)
-		log.Println("Server error, quitting")
-	}
-
-	go pingPong(&c, pingURL, *printPingInfo)
 	for {
 		select {
-		case cmd := <-shutdownChannel:
-			if cmd == SERVER_ERROR {
-				return
-			}
+		case <-ctx.Done():
+			return
 		default:
 
 			fmt.Println("Choose next action: /m - message, /p - plans, /n - new plan")
@@ -160,6 +91,69 @@ func main() {
 			default:
 				fmt.Printf("Unrecognised command: %c\n", cmd)
 			}
+		}
+	}
+}
+
+func main() {
+	printPingInfo := flag.Bool("d", false, "Log ping pong debug info")
+	flag.Parse()
+
+	SERVER_URL, _ := url.Parse("http://0.0.0.0:5000")
+	joinURL := SERVER_URL.JoinPath("/join/mock")
+	pingURL := SERVER_URL.JoinPath("/ping")
+
+	jar, _ := cookiejar.New(nil)
+	c := http.Client{
+		Timeout: PONG_TIMEOUT,
+		Jar:     jar,
+	}
+	defer c.CloseIdleConnections()
+	resp, err := c.Get(joinURL.String())
+	if err != nil {
+		log.Println(err)
+		log.Println("Server error, quitting")
+		return
+	}
+	c.Jar.SetCookies(joinURL, resp.Cookies())
+	buffer := make([]byte, 1024)
+	n, err := resp.Body.Read(buffer)
+	if err != io.EOF && n != 0 {
+		log.Println(err)
+		log.Println("Server error, quitting")
+	}
+
+	pingPongContext, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go interactWithUser(pingPongContext, &c)
+	for {
+		pong, err := c.Get(pingURL.String())
+
+		if err != nil {
+			log.Println(err)
+			log.Println("Server error, quitting")
+			return
+		}
+		var waiter chan []byte = make(chan []byte)
+		go (func() {
+			defer pong.Body.Close()
+			n, _ := pong.Body.Read(buffer)
+			if n != 0 {
+				waiter <- buffer[0:n]
+			}
+		})()
+		buf := <-waiter
+		msg := msg_pb.MsgResponse{}
+		err = proto.Unmarshal(buf, &msg)
+		if err == nil {
+			log.Printf("%d received msg %d: %s", msg.AuthorId, msg.Id, msg.Text)
+		} else if string(buf) == "pong" {
+			if *printPingInfo {
+				log.Printf("pong")
+			}
+		} else {
+			log.Print(err)
 		}
 	}
 }
