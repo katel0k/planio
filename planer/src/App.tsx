c0dacda58ad47e7b241d4d295a7e0d27615c7fd3
katel0k <katelkin2003@mail.ru>
2025-01-02 22:51:05 +0300

    Hooray its fully setup!

--- planer/src/App.tsx
+++ planer/src/App.tsx
@@ -1,25 +1,66 @@
-import { ReactNode } from 'react'
-import planPB from './protos/plan.proto'
+import { ReactNode, useState, useEffect } from 'react'
+import planPB from 'plan.proto'
 
 interface MessageProps {
     text: string,
     author: number
 }
 
-function MessageComponent({ text, author } : MessageProps): ReactNode {
-    fetch("localhost:5000/").then()
-    return  (
+function MessageComponent({ text, author }: MessageProps): ReactNode {
+    return (
         <div className="message-wrapper">
             <div className="message">
-                <div className="author"><span className="author">{author}</span></div>
+                <div className="author"><span>{author}</span></div>
                 <div className="text"><span>{text}</span></div>
             </div>
         </div>
-        )
+    )
+}
+
+interface PlanProps {
+    synopsis: string,
+    id: number
+}
+
+function PlanComponent({ synopsis, id }: PlanProps): ReactNode {
+    return (
+        <div className="plan-wrapper">
+            <div className="plan">
+                <div className="id"><span>{id}</span></div>
+                <div className="synopsis"><span>{synopsis}</span></div>
+            </div>
+        </div>
+    )
+}
+
+function Plans(): ReactNode {
+    const [agenda, setAgenda] = useState<planPB.plan.IAgenda>({});
+    useEffect(() => {
+        fetch("http://0.0.0.0:5000/plans")
+            .then(response => response.arrayBuffer())
+            .then(buffer => planPB.plan.Agenda.decode(new Uint8Array(buffer)))
+            .then(res => setAgenda(res))
+    });
+
+    return (
+        <div className="plans">
+            {agenda.plans?.map((props, index) =>
+                <PlanComponent
+                    synopsis={props.synopsis ?? ''}
+                    id={props.id ?? 0}
+                    key={index} />
+            )}
+        </div>
+    )
 }
 
 export default function App() {
     return (
-        <MessageComponent text="hi" author={1} />
+        <div className="wrapper">
+            <Plans />
+            <div className="messages">
+                <MessageComponent text={'Hi:)'} author={3} />
+            </div>
+        </div>
     )
 };
--- planer/src/index.tsx
+++ planer/src/index.tsx
@@ -7,11 +7,14 @@ const rootElement = document.getElementById("root");
 
 if (rootElement) {
     const root = createRoot(rootElement);
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
+    fetch("http://0.0.0.0:5000/join/artem")
+        .then(() => {
+            root.render(
+                <React.StrictMode>
+                    <App />
+                </React.StrictMode>
+            );
+        })
 }
 else {
     throw new Error("No root element!!!");
