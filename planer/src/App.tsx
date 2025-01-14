52f265fed14786317a89767a3c0f376aa370175e
katel0k <katelkin2003@mail.ru>
2025-01-15 00:22:09 +0300

    Auth component made

--- planer/src/App.tsx
+++ planer/src/App.tsx
@@ -1,48 +1,62 @@
-import { ReactNode, createContext} from 'react'
+import { createContext, ReactNode, useState } from 'react'
 import { join as joinPB } from 'join.proto'
 
-const id: number =
-    await fetch("http://0.0.0.0:5000/join", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(joinPB.JoinRequest.create({
-            username: "Artem"
-        }).toJSON()),
-    })
-        .then((response: Response) => response.arrayBuffer())
-        .then((buffer: ArrayBuffer) => joinPB.JoinResponse.decode(new Uint8Array(buffer)))
-        .then((res: joinPB.JoinResponse) => {
-            console.log(res.isNew);
-            return res.id;
-        })
-export const IdContext: React.Context<number> = createContext(id);
+const NAME_COOKIE_KEY: string = 'name';
+const ID_UNSET: number = -1;
+function getNameCookie(): number {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + NAME_COOKIE_KEY.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? parseInt(decodeURIComponent(matches[1])) : ID_UNSET;
+}
+    
+export const IdContext: React.Context<number> = createContext(ID_UNSET);
 
-import { Plans } from './Plan'
+import { Plans } from './Plan';
 import { Messenger } from './Message';
 
-function AppState(): ReactNode {
+function AuthComponent({ handleAuth }: { handleAuth: (nickname: string) => void }): ReactNode {
+    const [nickname, setNickname] = useState<string>('');
     return (
-        <></>
-    )
-}
-
-function Navbar(): ReactNode {
-    return (
-        <nav>
-            <AppState />
-        </nav>
+        <div className="auth">
+            <input type="text" value={nickname} onChange={e => setNickname(e.target.value)} />
+            <input type="submit" onClick={_ => handleAuth(nickname)} />
+        </div>
     )
 }
 
 export default function App() {
-
+    const [id, setId] = useState<number>(getNameCookie());
+    
+    if (id == ID_UNSET) {
+        return (
+            <AuthComponent handleAuth={
+                (nickname: string) => {
+                    fetch("http://0.0.0.0:5000/join", {
+                        method: "POST",
+                        headers: {
+                            'Content-Type': 'application/json;charset=utf-8'
+                        },
+                        body: JSON.stringify(joinPB.JoinRequest.create({
+                            username: nickname
+                        }).toJSON()),
+                    })
+                        .then((response: Response) => response.arrayBuffer())
+                        .then((buffer: ArrayBuffer) => joinPB.JoinResponse.decode(new Uint8Array(buffer)))
+                        .then((res: joinPB.JoinResponse) =>{
+                            console.log(res);
+                            setId(res.id)
+                        })
+                }
+            } />
+        )
+    }
     return (
-        <div className="wrapper">
-            <Navbar />
-            <Plans />
-            <Messenger />
-        </div>
+        <IdContext.Provider value={id}>
+            <div className="wrapper">
+                <Plans />
+                <Messenger />
+            </div>
+        </IdContext.Provider>
     )
 };
