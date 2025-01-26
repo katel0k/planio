f75c25e3ceb8801eca124f83a95859babfd615cb
katel0k <katelkin2003@mail.ru>
2025-01-26 22:28:02 +0300

    CLeaned up codestyle in database service

--- plango/server/databaseService.go
+++ plango/server/databaseService.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
-	msg_pb "github.com/katel0k/planio/server/build/msg"
-	plan_pb "github.com/katel0k/planio/server/build/plan"
+	msgPB "github.com/katel0k/planio/server/build/msg"
+	planPB "github.com/katel0k/planio/server/build/plan"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -68,9 +68,9 @@ func (db Database) CreateNewUser(username string) (int, error) {
 	}
 }
 
-func (db Database) CreateNewMessage(author_id int, receiver_id int, text string) (int, error) {
+func (db Database) CreateNewMessage(authorId int, receiverId int, text string) (int, error) {
 	row := db.Pool.QueryRow(context.Background(),
-		"INSERT INTO messages(author_id, receiver_id, body) VALUES ($1, $2, $3) RETURNING id", author_id, receiver_id, text)
+		"INSERT INTO messages(author_id, receiver_id, body) VALUES ($1, $2, $3) RETURNING id", authorId, receiverId, text)
 	var id int
 	err := row.Scan(&id)
 	if err != nil {
@@ -81,51 +81,51 @@ func (db Database) CreateNewMessage(author_id int, receiver_id int, text string)
 	return id, nil
 }
 
-func (db Database) GetAllMessages(req *msg_pb.AllMessagesRequest) (*msg_pb.AllMessagesResponse, error) {
+func (db Database) GetAllMessages(req *msgPB.AllMessagesRequest) (*msgPB.AllMessagesResponse, error) {
 	rows, err := db.Pool.Query(context.Background(),
 		"SELECT id, author_id, text FROM messages WHERE author_id=$1 AND receiver_id=$2", req.SenderId, req.ReceiverId)
-	var resp msg_pb.AllMessagesResponse
+	var resp msgPB.AllMessagesResponse
 	for rows.Next() {
-		var msg msg_pb.MsgResponse
+		var msg msgPB.MsgResponse
 		rows.Scan(&msg.Id, &msg.AuthorId, &msg.Text)
 		resp.Messages = append(resp.Messages, &msg)
 	}
 	return &resp, err
 }
 
-func (db Database) GetAllPlans(user_id int) (*plan_pb.Agenda, error) {
+func (db Database) GetAllPlans(userId int) (*planPB.Agenda, error) {
 	rows, err := db.Pool.Query(context.Background(),
 		`SELECT id, synopsis, creation_dttm, parent_id, scale, body as description
 		FROM plans FULL OUTER JOIN descriptions ON id=plan_id
-		WHERE author_id=$1`, user_id)
+		WHERE author_id=$1`, userId)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-	var agenda plan_pb.Agenda
+	var agenda planPB.Agenda
 	for rows.Next() {
-		var plan plan_pb.Plan
+		var plan planPB.Plan
 		var scale string
 		var creationTime time.Time
 		rows.Scan(&plan.Id, &plan.Synopsis, &creationTime, &plan.Parent, &scale, &plan.Description)
 		plan.CreationTime = timestamppb.New(creationTime)
-		plan.Scale = plan_pb.TimeScale(plan_pb.TimeScale_value[scale])
+		plan.Scale = planPB.TimeScale(planPB.TimeScale_value[scale])
 		agenda.Plans = append(agenda.Plans, &plan)
 	}
 	return &agenda, nil
 }
 
-func (db Database) CreateNewPlan(author_id int, plan *plan_pb.NewPlanRequest) (*plan_pb.Plan, error) {
+func (db Database) CreateNewPlan(authorId int, plan *planPB.NewPlanRequest) (*planPB.Plan, error) {
 	row := db.Pool.QueryRow(context.Background(),
 		`INSERT INTO plans(author_id, synopsis, parent_id, scale) VALUES ($1, $2, $3, $4)
 		RETURNING id, synopsis, creation_dttm, parent_id, scale`,
-		author_id, plan.Synopsis, plan.Parent, plan.Scale)
-	var res plan_pb.Plan
+		authorId, plan.Synopsis, plan.Parent, plan.Scale)
+	var res planPB.Plan
 	var creationTime time.Time
 	var scale string
 	err := row.Scan(&res.Id, &res.Synopsis, &creationTime, &res.Parent, &scale)
 	res.CreationTime = timestamppb.New(creationTime)
-	res.Scale = plan_pb.TimeScale(plan_pb.TimeScale_value[scale])
+	res.Scale = planPB.TimeScale(planPB.TimeScale_value[scale])
 	if err != nil {
 		log.Default().Print(err)
 		log.Default().Printf("Failed to add message in database")
@@ -134,7 +134,7 @@ func (db Database) CreateNewPlan(author_id int, plan *plan_pb.NewPlanRequest) (*
 	return &res, nil
 }
 
-func (db Database) ChangePlan(plan *plan_pb.ChangePlanRequest) error {
+func (db Database) ChangePlan(plan *planPB.ChangePlanRequest) error {
 	_, err := db.Pool.Exec(context.Background(), "UPDATE plans SET synopsis=$1 WHERE id=$2", plan.Synopsis, plan.Id)
 	return err
 }
